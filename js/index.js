16589f21710beea6c0af76cb63f7c96a1dcf03f1
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-03-12 17:16:41 +0300

    Updated code and added notifix

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-//import './css/styles.css';
-
 import { fetchUsers } from "./fetchUsers.js";
 
-const DEBOUNCE_DELAY = 300;
+const body = document.querySelector("body");
 
 const searchBox = document.querySelector("#search-box");
 
@@ -14,21 +12,24 @@ searchBox.style.marginLeft = '20px';
 
 searchBox.style.marginTop = '20px';
 
+const DEBOUNCE_DELAY = 300;
+
 function renderCountryIndexList(countries, index)
 {
     const markup = countries.filter((country) => country.name.common[0] === index).map((country) =>
-   {
-       return "<br/>" +
-             "<li class='country-item' style='list-style-type: none;'>" +
-               "<div style ='display: flex; align-items: center;'>" +
-                 `<a class='country-link' href='${country.flags.png}'>` +
-                   `<img class='country-image' src='${country.flags.svg}' width='50' height='30' title='{country.name.official}' alt='{country.name.official}'/>` +
-                 "</a>" +
-                 "<span style='margin-left: 10px;'>" +
-                 `<b>${country.name.common}</b></span>` +              
-              "</div>" +
-            "</li>";
-            
+    {
+        return "<br/>" +
+               "<li class='country-item'>" +
+                   "<div class='country-article'>" +
+                       `<a class='country-link' href='${country.flags.png}'>` +
+                           `<img class='country-image' src='${country.flags.svg}' width='50' height='30' title='{country.name.official}' alt='{country.name.official}'/>` +
+                       "</a>" +
+                       "<span>" +
+                           `<b class='country-title'>${country.name.common}</b>` +
+                       "</span>" +
+                   "</div>" +
+               "</li>";
+
     }).sort().join("");
     
     countryList.innerHTML += markup;
@@ -38,44 +39,44 @@ function renderCountryList(countries, text)
     const markup = countries.filter((country) => country.name.common === text).map((country) =>
     {
         return "<br/>" +
-               "<li class='country-item' style='list-style-type: none;'>" +
-                 "<div style='display: flex; align-items: center;'>" +                       
-                  "<b style='margin-right: 5px;'>Flag:</b>" +
-                   `<a class='country-link' href='${country.flags.png}'>` +
-                     `<img class='country-image' src='${country.flags.svg}' width='200' height='100' title='{country.name.official}' alt='{country.name.official}'/>` +
-                 "</a>" +
-               "</div>" +
-               "<br/>" +
-               "<p>" +
-                 "<b>Name country:&nbsp;&nbsp;</b>" +
-                 `<i class='country-name'>${country.name.official};</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Area:&nbsp;&nbsp;</b>" +
-                `<i class='country-area'>${country.area} m<sup>2</sup>;</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Continent:&nbsp;&nbsp;</b>" +
-                `<i class='country-region'>${country.continents};</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Subcontinent:&nbsp;&nbsp;</b>" +
-                `<i class='country-subregion'>${country.subregion};</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Population:&nbsp;&nbsp;</b>" +
-                `<i class='country-population'>${country.population} people;</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Capital:&nbsp;&nbsp;</b>" +
-                `<i class='country-capital'>${country.capital};</i>` +
-              "</p>" +
-              "<p>" +
-                "<b>Languages:&nbsp;&nbsp;</b>" +
-                `<i class='country-languages'>${Object.values(country.languages).join(", ")};</i>` +
-              "</p>" +
-              "<br/>" +
-            "</li>";
+               "<li class='country-item'>" +
+                   "<div class='country-article'>" +                       
+                       "<b class='country-label'>Flag:</b>" +
+                       `<a class='country-link' href='${country.flags.png}'>` +
+                           `<img class='country-image' src='${country.flags.svg}' width='200' height='100' title='{country.name.official}' alt='{country.name.official}'/>` +
+                       "</a>" +
+                   "</div>" +
+                   "<br/>" +
+                   "<p>" +
+                       "<b class='country-label'>Name country:</b>" +
+                       `<i class='country-name'>${country.name.official};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Area:</b>" +
+                       `<i class='country-area'>${country.area} m<sup>2</sup>;</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Continent:</b>" +
+                       `<i class='country-region'>${country.continents};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Subcontinent:</b>" +
+                       `<i class='country-subregion'>${country.subregion};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Population:</b>" +
+                       `<i class='country-population'>${country.population} people;</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Capital:</b>" +
+                       `<i class='country-capital'>${country.capital};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Languages:</b>" +
+                       `<i class='country-languages'>${Object.values(country.languages).join(", ")};</i>` +
+                   "</p>" +
+                   "<br/>" +
+               "</li>";
             
     }).sort().join("");
     
@@ -87,29 +88,38 @@ function clearList()
 }
 searchBox.addEventListener("input", ()=> 
 {
+    const countryItem = document.querySelector(".country-item");
+
     const target_element = event.currentTarget.value;
     
     const indexElement = target_element[0];
     
-    const indexName = document.querySelector("b.country-name");
-    
-    fetchUsers().then((country) =>
+    if (target_element === "")
     {
-        if (countryInfo !== "")
+        clearList();
+
+        body.setAttribute("onload", Notiflix.Notify.failure('Oops! There is no country!'));
+
+        //Too many maches found please ennter a specific name!
+        console.log("\nOops! There is no country!");
+    }
+    else
+    {
+        fetchUsers().then((country) =>
         {
             setTimeout(() =>
             {
                 renderCountryIndexList(country, indexElement);
-            
+              
             }, 1000);
-        
+          
             clearList();
-        
+      
             renderCountryList(country, target_element);
-       }
-       else
-       {
-           countryInfo.textContent = "Error!";
-       }
-    }).catch((error) => console.log(error));
+
+        }).catch((error) =>
+        {
+            console.log(error);
+        });
+    }
 });
