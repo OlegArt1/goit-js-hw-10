f042366f909c22b79eb0e31c62966b4d9c7552d6
OlegArt1 <113111922+OlegArt1@users.noreply.github.com>
2023-03-15 18:11:19 +0300

    Updated files index.js and fetchUsers.js

--- js/fetchUsers.js
+++ js/fetchUsers.js
@@ -1,9 +1,15 @@
+// Fetch users
+
 export function fetchUsers()
 {
     return fetch("https://restcountries.com/v3.1/all").then((response) =>
     {
         if (!response.ok)
         {
+            if (response.status === 404)
+            {
+                return [];
+            }
             throw new Error(response.status);
         }
         return response.json();
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// List Countries
+
 import { fetchUsers } from "./fetchUsers.js";
 
 const body = document.querySelector("body");
@@ -6,7 +8,9 @@ const searchBox = document.querySelector("#search-box");
 
 const countryList = document.querySelector(".country-list");
 
-const countryInfo = document.querySelector(".countrt-info");
+const countryInfo = document.querySelector(".country-info");
+
+const countryItem = document.querySelector(".country-item-list");
 
 searchBox.style.marginLeft = '40px';
 
@@ -14,65 +18,53 @@ searchBox.style.marginTop = '30px';
 
 const DEBOUNCE_DELAY = 300;
 
-function renderCountryIndexList(countries, index)
+function renderCountryList(countries, index)
 {
-    const markup = countries.filter((country) => country.name.common[0] === index).map((country) =>
+    const markup_list = countries.filter((country) => country.name.common[0] === index).map((country) =>
     {
         return "<br/>" +
                "<li class='country-item-list'>" +
-                   "<div class='country-article'>" +
+                   "<article class='country-article'>" +
                        `<a class='country-link' href='${country.flags.png}'>` +
                            `<img class='country-image' src='${country.flags.svg}' width='50' height='30' title='{country.name.official}' alt='{country.name.official}'/>` +
+                           "<span>" +
+                               `<b class='country-title'>${country.name.common}</b>` +
+                           "</span>" +
                        "</a>" +
-                       "<span>" +
-                           `<b class='country-title'>${country.name.common}</b>` +
-                       "</span>" +
-                   "</div>" +
+                   "</article>" +
                "</li>";
 
     }).sort().join("");
     
-    countryList.innerHTML += markup;
+    countryList.innerHTML = markup_list;
 }
-function renderCountryList(countries, text)
+function renderCountryIndexList(countries, text)
 {
     const markup = countries.filter((country) => country.name.common === text).map((country) =>
     {
         return "<br/>" +
                "<li class='country-item'>" +
-                   "<div class='country-article'>" +                       
-                       "<b class='country-label'>Flag:&nbsp;&nbsp;</b>" +
+                   "<article class='country-article'>" +                       
+                       "<b class='country-flag'>Flag:</b>" +
                        `<a class='country-link' href='${country.flags.png}'>` +
-                           `<img class='country-image' src='${country.flags.svg}' width='200' height='100' title='{country.name.official}' alt='{country.name.official}'/>` +
+                           `<img class='country-flag-image' src='${country.flags.svg}' width='200' height='100' title='{country.name.official}' alt='{country.name.official}'/>` +
                        "</a>" +
-                   "</div>" +
+                   "</article>" +
                    "<br/>" +
                    "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Name country:</b>" +
+                       "<b class='country-label'>Name country:</b>" +
                        `<i class='country-name'>${country.name.official};</i>` +
                    "</p>" +
                    "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Area:</b>" +
-                       `<i class='country-area'>${country.area} m<sup>2</sup>;</i>` +
-                   "</p>" +
-                   "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Continent:</b>" +
-                       `<i class='country-region'>${country.continents};</i>` +
-                   "</p>" +
-                   "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Subcontinent:</b>" +
-                       `<i class='country-subregion'>${country.subregion};</i>` +
-                   "</p>" +
-                   "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Population:</b>" +
+                       "<b class='country-label'>Population:</b>" +
                        `<i class='country-population'>${country.population} people;</i>` +
                    "</p>" +
                    "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Capital:</b>" +
+                       "<b class='country-label'>Capital:</b>" +
                        `<i class='country-capital'>${country.capital};</i>` +
                    "</p>" +
                    "<p>" +
-                       "<b class='country-label'>&nbsp;&nbsp;&nbsp;&nbsp;Languages:</b>" +
+                       "<b class='country-label'>Languages:</b>" +
                        `<i class='country-languages'>${Object.values(country.languages).join(", ")};</i>` +
                    "</p>" +
                    "<br/>" +
@@ -80,46 +72,100 @@ function renderCountryList(countries, text)
             
     }).sort().join("");
     
-    countryList.innerHTML = markup;
+    countryInfo.innerHTML = markup;
+}
+function renderCountryIndexAllList(countries, text)
+{
+    const markup = countries.filter((country) => country.name.common === text).map((country) =>
+    {
+        return "<br/>" +
+               "<li class='country-item'>" +
+                   "<div class='country-article'>" +                       
+                       "<b class='country-label'>Flag:</b>" +
+                       `<a class='country-link' href='${country.flags.png}'>` +
+                           `<img class='country-image' src='${country.flags.svg}' width='200' height='100' title='{country.name.official}' alt='{country.name.official}'/>` +
+                       "</a>" +
+                   "</div>" +
+                   "<br/>" +
+                   "<p>" +
+                       "<b class='country-label'>Name country:</b>" +
+                       `<i class='country-name'>${country.name.official};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Area:</b>" +
+                       `<i class='country-area'>${country.area} m<sup>2</sup>;</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Continent:</b>" +
+                       `<i class='country-region'>${country.continents};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Subcontinent:</b>" +
+                       `<i class='country-subregion'>${country.subregion};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Population:</b>" +
+                       `<i class='country-population'>${country.population} people;</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Capital:</b>" +
+                       `<i class='country-capital'>${country.capital};</i>` +
+                   "</p>" +
+                   "<p>" +
+                       "<b class='country-label'>Languages:</b>" +
+                       `<i class='country-languages'>${Object.values(country.languages).join(", ")};</i>` +
+                   "</p>" +
+                   "<br/>" +
+               "</li>";
+            
+    }).sort().join("");
+    
+    countryInfo.innerHTML = markup;
 }
 function clearList()
 {
     countryList.innerHTML = "";
+
+    countryInfo.innerHTML = "";
 }
 searchBox.addEventListener("input", ()=> 
 {
-    const countryItem = document.querySelector(".country-name");
-
+    const target_index_element = searchBox.value.trim();
+    
     const target_element = event.currentTarget.value;
     
-    const indexElement = target_element[0];
+    const index_element = target_element[0];
     
-    if (target_element === "")
+    fetchUsers().then((country) =>
     {
-        clearList();
-
-        body.setAttribute("onload", Notiflix.Notify.failure('Ошибка! Пустая строка!'));
-
-        //Too many maches found please ennter a specific name! Oops! There is no country!
-        console.log("\nOops! There is no country!");
-    }
-    else
-    {
-        fetchUsers().then((country) =>
+        setTimeout(() =>
         {
-            setTimeout(() =>
+            if (target_index_element.length >= 1 && target_index_element.length <= 10)
             {
-                renderCountryIndexList(country, indexElement);
-              
-            }, DEBOUNCE_DELAY);
-          
-            clearList();
-      
-            renderCountryList(country, target_element);
+                countryInfo.innerHTML = "";
 
-        }).catch((error) =>
-        {
-            console.log(error);
-        });
-    }
+                renderCountryList(country, index_element);
+            }
+            else if (target_index_element.length > 10)
+            {
+                body.setAttribute("onload", Notiflix.Notify.failure('Too many maches found! Please enter are more specific name!'));
+
+                console.log("\nToo many maches found! Please enter are more specific name!");
+            }
+            else if (target_index_element.length === 0)
+            {
+                clearList();
+                
+                body.setAttribute("onload", Notiflix.Notify.failure('Oops, there is no country with that name!'));
+                    
+                console.log("\nOops, there is no country with that name!");
+            }
+            renderCountryIndexList(country, target_element);
+        
+        }, DEBOUNCE_DELAY);
+
+    }).catch((error) =>
+    {
+        console.log(error);
+    });
 });
